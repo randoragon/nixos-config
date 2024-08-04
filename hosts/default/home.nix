5adabf605fd0dc322bbd38ee632e9e95080dc846
randoragon <randoragongamedev@gmail.com>
2024-08-04 13:07:11 +0200

    feat(HM): Add sessionVariables to home.nix, cleanup

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   imports = [
     ../../modules/home-manager
   ];
 
   home.username = "pcache";
-  home.homeDirectory = "/home/pcache";
+  home.homeDirectory = "/home/${config.home.username}";
 
   home.packages = with pkgs; [
     # Development tools
@@ -78,40 +78,24 @@
     # '')
   ];
 
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/pcache/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
+    TERMINAL = "foot";
+    BROWSER = "firefox";
+    PAGER = "less";
+    PDF_READER = "zathura";
+
+    # This makes markdown previews with tables bearable
+    # to look at, just prepend the html file with it
+    HTML_MD_STYLE = ''
+      <style>
+      table, th, td {
+          padding: 5px;
+          border: 1px solid black;
+          border-collapse: collapse;
+      }
+      </style>
+    '';
   };
 
   programs.home-manager.enable = true;
