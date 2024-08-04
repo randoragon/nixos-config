f231de82ce8badedc416ba9f6f22fbb8f30911a9
randoragon <randoragongamedev@gmail.com>
2024-08-04 13:08:57 +0200

    feat: Add XDG directories and zsh tweak to shell.nix

--- modules/nixos/shell.nix
+++ modules/nixos/shell.nix
@@ -7,4 +7,22 @@
     enable = true;
     syntaxHighlighting.enable = true;
   };
+
+  # Enable completion for system packages
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zsh.enableCompletion
+  environment.pathsToLink = [ "/share/zsh" ];
+
+  # System environment variables
+  environment.sessionVariables = rec {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME = "$HOME/.local/bin";
+    PATH = [
+      "${XDG_BIN_HOME}"
+    ];
+  };
 }
