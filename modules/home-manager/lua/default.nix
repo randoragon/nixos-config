291e6a65893c309a71e5377cc4b80e6616c78635
randoragon <randoragongamedev@gmail.com>
2024-08-06 01:58:12 +0200

    fix(HM/lua): Export env var

--- modules/home-manager/lua/default.nix
+++ modules/home-manager/lua/default.nix
@@ -1,4 +1,8 @@
-{
+{ config, ... }: {
+  home.sessionVariables = {
+    LUA_INIT = "@${config.xdg.configHome}/lua/init.lua";
+  };
+
   xdg.configFile."lua/init.lua" = {
     source = ./init.lua;
   };
