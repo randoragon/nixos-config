de4c45b7695d1fa63cd0e8675f84a713d0b8ffb8
randoragon <randoragongamedev@gmail.com>
2024-08-06 01:19:08 +0200

    fix(HM/wget): Missing env export

--- modules/home-manager/wget/default.nix
+++ modules/home-manager/wget/default.nix
@@ -1,3 +1,7 @@
 { config, ... }: {
+  home.sessionVariables = {
+    WGETRC = "${config.xdg.configHome}/wgetrc";
+  };
+
   xdg.configFile."wgetrc".text = "hsts-file=${config.xdg.cacheHome}/wget-hsts";
 }
