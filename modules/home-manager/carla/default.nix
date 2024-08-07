68fe832993d253ed961f7cb63e8080e57dbf50e2
randoragon <randoragongamedev@gmail.com>
2024-08-07 17:37:20 +0200

    feat(HM/carla): Generate a writeable config file

--- modules/home-manager/carla/default.nix
+++ modules/home-manager/carla/default.nix
@@ -1,5 +1,17 @@
-{
-  xdg.configFile."falkTX/Carla2.conf" = {
-    source = ./Carla2.conf;
+{ config, lib, ... }: {
+  # Disable carla confirmation dialog on quit.
+  # Use activation, because we want the config file to be writeable.
+  home.activation = let
+    config_dir = "${config.xdg.configHome}/falkTX";
+    config_file = "${config_dir}/Carla2.conf";
+  in {
+    carlaDontAskToQuit = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      run sh -c '
+        if [ ! -f "${config_file}" ]; then
+        mkdir -p -- "${config_dir}"
+        printf "[Main]\nConfirmExit=false\n" >"${config_file}"
+      fi
+      '
+    '';
   };
 }
