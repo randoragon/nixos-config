8f12b59312bb57a95718b86daec8ffe052cebfa7
randoragon <randoragongamedev@gmail.com>
2024-08-21 12:55:18 +0200

    refactor(HM): Remove default comment from home.nix

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -65,13 +65,6 @@
     figlet sl asciiquarium
     newsraft
     onedrive
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   home.sessionVariables = {
