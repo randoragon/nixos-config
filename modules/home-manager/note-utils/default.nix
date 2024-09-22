41dd20e82ee12aa64dff1d9a33c9609685b4519d
randoragon <randoragongamedev@gmail.com>
2024-09-22 18:01:52 +0200

    feat(scripts): Delete rgnote
    Not needed anymore after the latest structural changes.

--- modules/home-manager/note-utils/default.nix
+++ modules/home-manager/note-utils/default.nix
@@ -1,5 +1,4 @@
 {
   home.file.".local/bin/note".source = ./localbin/note;
-  home.file.".local/bin/rgnote".source = ./localbin/rgnote;
   home.file.".local/bin/tml-get-artist-links".source = ./localbin/tml-get-artist-links;
 }
