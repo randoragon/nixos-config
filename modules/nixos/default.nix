ead8b42d82d7763a4669ea17d8347bb1fd4af6ea
randoragon <randoragongamedev@gmail.com>
2024-10-29 12:16:58 +0100

    feat: Install systemd services for using sshd

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   imports = [
     ./boot.nix
     ./fonts.nix
@@ -13,6 +13,13 @@
   services.udisks2.enable = true;
   programs.ssh.startAgent = true;
   services.ntp.enable = true;
+
+  # Create the SSH services, but do not enable them. This does not install ssh
+  # or sshd, they are available without these lines. But it does create systemd
+  # services like sshd.service that make it easy to start an SSH server.
+  # https://www.reddit.com/r/NixOS/comments/16mbn41/install_but_dont_enable_openssh_sshd_service/
+  services.openssh.enable = true;
+  systemd.services.sshd.wantedBy = lib.mkForce [ ];
 
   programs.gnupg.agent = {
     enable = true;
