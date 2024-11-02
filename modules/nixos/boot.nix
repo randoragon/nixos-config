2fda37345255e182c32ced1d46d0009ce02a553d
randoragon <randoragongamedev@gmail.com>
2024-11-02 13:26:20 +0100

    feat: Use latest linux kernel instead of LTS
    On nixbox, the NVIDIA driver seems to be freaking out about displays, as
    it registered "Unknown-1" which doesn't exist.
    To fix, this requires some hacking in an autostart script:
    
        wlr-randr --output Unknown-1 --off \
            --output HDMI-A-1 --on --pos 0,0 --preferred
    
    I may fall back to LTS for nixbox if I find more issues, it seems rather
    unstable.

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,4 +1,5 @@
-{
+{ pkgs, ... }: {
+  boot.kernelPackages = pkgs.linuxPackages_latest;  # Nix uses LTS kernel by default; use latest
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.tmp.useTmpfs = true;
