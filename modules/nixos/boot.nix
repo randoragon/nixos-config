8c68822a557f9fa08a4828190c88557de328dad9
randoragon <randoragongamedev@gmail.com>
2024-11-14 00:26:04 +0100

    fix: Use LTS kernel instead of latest
    This reverts 2fda37345255 (feat: Use latest linux kernel instead of LTS).
    
    The reasons are glitchy behavior that wasn't present on LTS:
    - fullscreen stutters, particularly in mpv
    - worse NVIDIA drivers behavior (bad font scaling in TTY, glitches with
      screen refreshing that require the TTY switching trick)
    
    I have not noticed a single improvement by switching to latest, only
    drawbacks. Therefore, I am switching back.

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }: {
-  boot.kernelPackages = pkgs.linuxPackages_latest;  # Nix uses LTS kernel by default; use latest
+{
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.tmp.useTmpfs = true;
