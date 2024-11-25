79111da5997db4f0a320b8f66d460320c02425b1
randoragon <randoragongamedev@gmail.com>
2024-11-26 00:14:08 +0100

    feat(sync-utils): Replace onedrive with rclone
    Also revamped the spush script a little

--- modules/home-manager/sync-utils/default.nix
+++ modules/home-manager/sync-utils/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ syncthing onedrive ];
+  home.packages = with pkgs; [ syncthing rclone ];
 
   home.file.".local/bin/spush".source = ./localbin/spush;
   home.file.".local/bin/spull".source = ./localbin/spull;
