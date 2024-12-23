42e462cf8b006a44a32ce0ff12553cfbb55ba96f
randoragon <randoragongamedev@gmail.com>
2024-12-23 21:56:01 +0100

    feat(river): Add float rules for Ardour and sfizz

--- modules/home-manager/river/river/init.d/0-rules.sh
+++ modules/home-manager/river/river/init.d/0-rules.sh
@@ -8,6 +8,10 @@ riverctl rule-add -app-id 'vesktop' tags $((1 << 11))
 
 # Floats
 riverctl rule-add -app-id 'floatme' float
+riverctl rule-add -title 'Load SFZ file' float  # sfizz dialog
+riverctl rule-add -title 'Rename Track' float  # Ardour dialog
+riverctl rule-add -title 'Color Selection:*' float  # Ardour dialog
+riverctl rule-add -title '* - Meterbridge - Ardour' float  # Ardour dialog
 
 # Server-side decorations
 riverctl rule-add -app-id 'firefox' ssd
