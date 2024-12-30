c221743319240c877d231246c6b875cda51ba936
randoragon <randoragongamedev@gmail.com>
2024-12-30 13:06:16 +0100

    refactor(carla): Rewrite activation in a prettier way
    Using builtins.replaceStrings instead of sed inside a shell script.

--- modules/home-manager/carla/default.nix
+++ modules/home-manager/carla/default.nix
@@ -1,20 +1,30 @@
-{ config, pkgs, lib, ... }: {
+{ config, lib, pkgs, ... }: {
   home.packages = with pkgs; [ carla ];
 
-  # Unfortunately the sessionVariables are not visible from the activation
-  # script. Perhaps there's a way to achieve it, but I was desperate for
-  # a working solution so I resorted to manually exporting everything.
-  home.activation = {
+  # Disable carla quit confirmation dialog and configure paths.
+  # Use activation, because we want the config file to be writeable.
+  home.activation = let
+    configDir = "${config.xdg.configHome}/falkTX";
+    configFile = "${configDir}/Carla2.conf";
+    configFileContent = ''
+      [Main]
+      ConfirmExit=false
+
+      [Paths]
+      DSSI=${builtins.replaceStrings [":"] [", "] config.home.sessionVariables.DSSI_PATH}
+      LADSPA=${builtins.replaceStrings [":"] [", "] config.home.sessionVariables.LADSPA_PATH}
+      LV2=${builtins.replaceStrings [":"] [", "] config.home.sessionVariables.LV2_PATH}
+      VST2=${builtins.replaceStrings [":"] [", "] config.home.sessionVariables.VST2_PATH}
+      VST3=${builtins.replaceStrings [":"] [", "] config.home.sessionVariables.VST3_PATH}
+      JSFX=${builtins.replaceStrings [":"] [", "] config.home.sessionVariables.JSFX_PATH}
+    '';
+  in {
     carlaInitialConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
       run sh -c '
-          export DSSI_PATH="${config.home.sessionVariables.DSSI_PATH}"
-          export LADSPA_PATH="${config.home.sessionVariables.LADSPA_PATH}"
-          export LV2_PATH="${config.home.sessionVariables.LV2_PATH}"
-          export LXVST_PATH="${config.home.sessionVariables.LXVST_PATH}"
-          export VST2_PATH="${config.home.sessionVariables.VST2_PATH}"
-          export VST3_PATH="${config.home.sessionVariables.VST3_PATH}"
-          export JSFX_PATH="${config.home.sessionVariables.JSFX_PATH}"
-          ${builtins.toPath ./setup.sh}
+        if [ ! -f "${configFile}" ]; then
+          mkdir -p -- "${configDir}"
+          echo "${configFileContent}" >"${configFile}"
+        fi
       '
     '';
   };
