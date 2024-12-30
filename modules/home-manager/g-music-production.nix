cbba8d60ce771d73466118b1cbe2eaed3b13a5fe
randoragon <randoragongamedev@gmail.com>
2024-12-30 01:32:52 +0100

    feat(g-music-production): Update plugin path envs
    Added paths into XDG_PRODUCTION_DIR and tidied up VST2 and VST3.

--- modules/home-manager/g-music-production.nix
+++ modules/home-manager/g-music-production.nix
@@ -1,5 +1,5 @@
 # Config group for systems with music production support.
-{ pkgs, spkgs, ... }: {
+{ config, pkgs, spkgs, ... }: {
   imports = [
     ./lmms
   ];
@@ -28,11 +28,15 @@
   ];
 
   # https://discourse.nixos.org/t/audio-plugins-no-longer-detected-after-switching-to-nix-2-4-flakes/17177/4
-  home.sessionVariables = {
-    DSSI_PATH = "$HOME/.dssi:$HOME/.nix-profile/lib/dssi:/run/current-system/sw/lib/dssi:/etc/profiles/per-user/$USER/lib/dssi";
-    LADSPA_PATH = "$HOME/.ladspa:$HOME/.nix-profile/lib/ladspa:/run/current-system/sw/lib/ladspa:/etc/profiles/per-user/$USER/lib/ladspa";
-    LV2_PATH = "$HOME/.lv2:$HOME/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2:/etc/profiles/per-user/$USER/lib/lv2";
-    LXVST_PATH = "$HOME/.lxvst:$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst:/etc/profiles/per-user/$USER/lib/lxvst";
-    VST_PATH = "$HOME/.vst:$HOME/.nix-profile/lib/vst:/run/current-system/sw/lib/vst:/etc/profiles/per-user/$USER/lib/vst";
+  home.sessionVariables = let
+    pluginsPath = "${config.xdg.userDirs.extraConfig.XDG_PRODUCTION_DIR}/000_audio-bank/plugins";
+  in {
+    DSSI_PATH = "${pluginsPath}/dssi:$HOME/.nix-profile/lib/dssi:/run/current-system/sw/lib/dssi:/etc/profiles/per-user/$USER/lib/dssi";
+    LADSPA_PATH = "${pluginsPath}/ladspa:$HOME/.nix-profile/lib/ladspa:/run/current-system/sw/lib/ladspa:/etc/profiles/per-user/$USER/lib/ladspa";
+    LV2_PATH = "${pluginsPath}/lv2:$HOME/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2:/etc/profiles/per-user/$USER/lib/lv2";
+    LXVST_PATH = "$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst:/etc/profiles/per-user/$USER/lib/lxvst";
+    VST2_PATH = "${pluginsPath}/vst2:$HOME/.nix-profile/lib/vst2:$HOME/.nix-profile/lib/vst:/run/current-system/sw/lib/vst2:/run/current-system/sw/lib/vst:/etc/profiles/per-user/$USER/lib/vst2:/etc/profiles/per-user/$USER/lib/vst";
+    VST3_PATH = "${pluginsPath}/vst3:$HOME/.nix-profile/lib/vst3:/run/current-system/sw/lib/vst3:/etc/profiles/per-user/$USER/lib/vst3";
+    JSFX_PATH = "$HOME/.config/REAPER/Effects";
   };
 }
