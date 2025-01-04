51374837c395d84521f489c51964fe247a957a51
randoragon <randoragongamedev@gmail.com>
2025-01-04 22:01:57 +0100

    feat(river): Even more Ardour floating rules

--- modules/home-manager/river/river/init.d/0-rules.sh
+++ modules/home-manager/river/river/init.d/0-rules.sh
@@ -9,14 +9,21 @@ riverctl rule-add -app-id 'vesktop' tags $((1 << 11))
 # Floats
 riverctl rule-add -app-id 'floatme' float
 riverctl rule-add -title 'Load SFZ file' float  # sfizz dialog
-riverctl rule-add -title 'Rename Track' float  # Ardour dialog
-riverctl rule-add -title 'Color Selection:*' float  # Ardour dialog
-riverctl rule-add -title '* - Meterbridge - Ardour' float  # Ardour dialog
-riverctl rule-add -title 'Customize port name' float  # Ardour dialog
-riverctl rule-add -title 'New Playlist for ALL Tracks' float  # Ardour dialog
-riverctl rule-add -title 'New Playlist for *' float  # Ardour dialog
-riverctl rule-add -title 'Copy Playlist for *' float  # Ardour dialog
-riverctl rule-add -title 'Rename Playlist' float  # Ardour dialog
+
+# Ardour floating dialogs
+riverctl rule-add -title 'Rename Track' float
+riverctl rule-add -title 'Color Selection:*' float
+riverctl rule-add -title '* - Meterbridge - Ardour' float
+riverctl rule-add -title 'Customize port name' float
+riverctl rule-add -title 'New Playlist for ALL Tracks' float
+riverctl rule-add -title 'New Playlist for *' float
+riverctl rule-add -title 'Copy Playlist for *' float
+riverctl rule-add -title 'Rename Playlist' float
+riverctl rule-add -title 'Rename Session' float
+riverctl rule-add -title 'Ardour: Locked' float
+riverctl rule-add -title 'Choose session to import metadata from' float
+riverctl rule-add -title 'Ardour: Log' float
+riverctl rule-add -title 'Rename Mark' float
 
 # Server-side decorations
 riverctl rule-add -app-id 'firefox' ssd
