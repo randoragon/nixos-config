70310690a5024d16db3de4a507b56343bb7d0676
randoragon <randoragongamedev@gmail.com>
2025-01-15 22:53:48 +0100

    feat(hledger): Add pozyczki subcommand
    For printing some personal summaries.

--- modules/home-manager/hledger/default.nix
+++ modules/home-manager/hledger/default.nix
@@ -7,6 +7,7 @@
 
   home.file.".local/bin/hle".source = ./hle;
   home.file.".local/bin/hledger-bsee".source = ./hledger-bsee;
+  home.file.".local/bin/hledger-pozyczki".source = ./hledger-pozyczki;
 
   home.sessionVariables = {
     LEDGER_FILE = "${config.xdg.dataHome}/ledger/secrets/main.journal";
