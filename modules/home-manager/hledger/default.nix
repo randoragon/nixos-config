81970be823afe0fb99f90b7ca9f0b509818cd0d2
randoragon <randoragongamedev@gmail.com>
2025-01-15 20:59:18 +0100

    feat(hledger): Use LEDGER_FILE env
    Makes everything a bit simpler. I don't remember why I avoided using it
    in the first place, maybe for security, but it's a minor thing.

--- modules/home-manager/hledger/default.nix
+++ modules/home-manager/hledger/default.nix
@@ -7,4 +7,8 @@
 
   home.file.".local/bin/hle".source = ./hle;
   home.file.".local/bin/hledger-bsee".source = ./hledger-bsee;
+
+  home.sessionVariables = {
+    LEDGER_FILE = "${config.xdg.dataHome}/ledger/secrets/main.journal";
+  };
 }
