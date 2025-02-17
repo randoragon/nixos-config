39b1a678d9bde8a33eb66a140932b3b6e64b08f4
randoragon <randoragongamedev@gmail.com>
2025-02-17 01:51:59 +0100

    feat(zsh): Open yazi on Ctrl-O
    Trying to migrate...

--- modules/home-manager/zsh/zshrc-extra-first.zsh
+++ modules/home-manager/zsh/zshrc-extra-first.zsh
@@ -51,17 +51,8 @@ setopt shwordsplit
 DIRSTACKSIZE=8
 setopt autopushd pushdminus pushdsilent pushdtohome
 
-# Use lf to switch directories and bind it to ctrl-o
-lfcd () {
-    tmp="$(mktemp)"
-    lf -last-dir-path="$tmp" "$@"
-    if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
-        rm -f "$tmp"
-        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
-    fi
-}
-bindkey -s '^o' 'lfcd\n'
+# Open yazicd on Ctrl-O
+bindkey -s '^o' 'yazicd\n'
 
 # Enable light-up menu
 zstyle ':completion:*' menu select
