12ef1e9e5c90cda5ee566165f33cad8a87355e7d
randoragon <randoragongamedev@gmail.com>
2025-02-19 13:40:11 +0100

    feat(hledger): Add hledger-bud subcommand

--- modules/home-manager/hledger/default.nix
+++ modules/home-manager/hledger/default.nix
@@ -8,6 +8,7 @@
   home.file.".local/bin/hle".source = ./hle;
   home.file.".local/bin/hledger-bsee".source = ./hledger-bsee;
   home.file.".local/bin/hledger-pozyczki".source = ./hledger-pozyczki;
+  home.file.".local/bin/hledger-bud".source = ./hledger-bud;
 
   home.sessionVariables = {
     LEDGER_FILE = "${config.xdg.dataHome}/ledger/secrets/main.journal";
