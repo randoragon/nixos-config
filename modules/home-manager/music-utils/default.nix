59dc9bba854afcc4876050c7da29eff3fa879708
randoragon <randoragongamedev@gmail.com>
2025-03-02 16:09:01 +0100

    fix: Remove plfilter command
    It has been superseded by the plfilter from music-tools.
    See https://github.com/randoragon/music-tools

--- modules/home-manager/music-utils/default.nix
+++ modules/home-manager/music-utils/default.nix
@@ -6,7 +6,6 @@
   home.file.".local/bin/pl2wavs".source = ./localbin/pl2wavs;
   home.file.".local/bin/pladd".source = ./localbin/pladd;
   home.file.".local/bin/pldel".source = ./localbin/pldel;
-  home.file.".local/bin/plfilter".source = ./localbin/plfilter;
   home.file.".local/bin/regain".source = ./localbin/regain;
   home.file.".local/bin/scanfairuse".source = ./localbin/scanfairuse;
 
@@ -20,6 +19,5 @@
   home.file.".scripts/next-playing".source = ./scripts/next-playing;
   home.file.".scripts/now-playing".source = ./scripts/now-playing;
   home.file.".scripts/pladd_select".source = ./scripts/pladd_select;
-  home.file.".scripts/plfilter_filter.lua".source = ./scripts/plfilter_filter.lua;
   home.file.".scripts/rate-playing".source = ./scripts/rate-playing;
 }
