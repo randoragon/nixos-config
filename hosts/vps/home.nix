a5aad5bbbbf3e75914618c11c337183f1fe5ca6b
randoragon <randoragongamedev@gmail.com>
2025-03-04 23:05:55 +0100

    fix(vps): Disable WM/tmux after login

--- hosts/vps/home.nix
+++ hosts/vps/home.nix
@@ -1,10 +1,13 @@
-{ pkgs, config, ... }: {
+{ lib, pkgs, config, ... }: {
   imports = [
     ../../modules/home-manager
   ];
 
   home.username = "pcache";
   home.homeDirectory = "/home/${config.home.username}";
+
+  # Disable running the WM or tmux after login
+  programs.zsh.loginExtra = lib.mkForce "";
 
   programs.home-manager.enable = true;
   home.stateVersion = "24.11";
