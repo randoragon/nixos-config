a5eb43f0e07c7a4ae6413acdf059799f5e466ab9
randoragon <randoragongamedev@gmail.com>
2025-03-04 18:00:39 +0100

    fix(vps): Use MBR + Grub + on-disk TMPDIR
    Vultr does not support UEFI, it seems.

---
+++ hosts/vps/boot.nix
@@ -0,0 +1,10 @@
+{ lib, ... }: {
+  # Overwrite boot options; use MBR, grub and on-disk TMPDIR
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = false;
+  };
+  boot.tmp.useTmpfs = lib.mkForce false;
+}
--- hosts/vps/configuration.nix
+++ hosts/vps/configuration.nix
@@ -2,6 +2,7 @@
 
   imports = [
     ./hardware-configuration.nix
+    ./boot.nix
     ../../modules/nixos
   ];
 
