c66a8c3d9958753437b84a544f0df8f05e647854
randoragon <randoragongamedev@gmail.com>
2025-03-16 14:58:56 +0100

    feat(vps): Add radicale auth
    I thought I would be tunneling over SSH, but it's more conventional (and
    thus more supported by 3rd party apps) to do HTTP basic auth.

--- hosts/vps/radicale.nix
+++ hosts/vps/radicale.nix
@@ -1,10 +1,17 @@
-{
+{ pkgs, ... }: {
+  # Needed for the htpasswd utility
+  environment.systemPackages = with pkgs; [ apacheHttpd ];
+
   services.radicale = {
     enable = true;
     settings = {
       server.hosts = [ "0.0.0.0:5232" ];
       # We will be using an nginx reverse proxy for security
-      auth.type = "none";
+      auth = {
+        type = "htpasswd";
+        htpasswd_filename = "/etc/radicale/users";
+        htpasswd_encryption = "bcrypt";
+      };
     };
   };
 }
