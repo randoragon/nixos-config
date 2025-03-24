918abefbcc5af057d660d75678369776f3b00ba0
randoragon <randoragongamedev@gmail.com>
2025-03-24 08:23:02 +0100

    feat(nixbox): Change SSH port for nixbox

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -35,6 +35,7 @@
     };
   };
 
+  services.openssh.ports = [2137];
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
