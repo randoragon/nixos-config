2c7f10db91e9cfaf449898c038aab1fe95dcdb8b
randoragon <randoragongamedev@gmail.com>
2025-03-31 01:14:16 +0200

    feat(vimiv): Use fit scaling by default

--- modules/home-manager/vimiv-qt/default.nix
+++ modules/home-manager/vimiv-qt/default.nix
@@ -3,4 +3,14 @@
 
   xdg.configFile."vimiv-qt/styles/base16-grayscale-dark".source = ./base16-grayscale-dark;
   xdg.configFile."vimiv-qt/vimiv.conf".source = ./vimiv.conf;
+
+  # Use fit scaling by default
+  home.file.".local/bin/vimiv" = {
+    text = ''
+      #!/bin/sh -e
+
+      ${pkgs.vimiv-qt}/bin/vimiv --command "scale --level=fit" "$@"
+    '';
+    executable = true;
+  };
 }
