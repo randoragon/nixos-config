bf04533bfd0e4749ebbd4dd94418663d3742dbfc
randoragon <randoragongamedev@gmail.com>
2025-05-10 13:43:46 +0200

    refactor(mako): Use new home manager options

--- modules/home-manager/mako/default.nix
+++ modules/home-manager/mako/default.nix
@@ -3,6 +3,34 @@
 
   services.mako = {
     enable = true;
-    extraConfig = builtins.readFile ./config;
+    settings = {
+      layer = "overlay";
+      max-history = 100;
+      max-visible = 5;
+
+      margin = 3;
+      text-alignment = "center";
+      border-size = 0;
+      border-radius = 4;
+      default-timeout = 5000;
+      background-color = "#328fbe";
+      text-color = "#000000";
+    };
+
+    criteria = {
+      "urgency=low" = {
+        default-timeout = 3000;
+        background-color = "#303030";
+        text-color = "#aaaaaa";
+      };
+
+      "urgency=critical" = {
+        border-size = 2;
+        border-color = "#220000";
+        default-timeout = 0;
+        background-color = "#bb2222";
+        text-color = "#ffffff";
+      };
+    };
   };
 }
