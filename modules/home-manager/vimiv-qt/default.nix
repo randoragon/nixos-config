fb403ad42c67526ef09029f9b3e5933bfd3e3f59
randoragon <randoragongamedev@gmail.com>
2025-06-01 12:12:25 +0200

    style: Use spaces around [ nix lists ]

--- modules/home-manager/vimiv-qt/default.nix
+++ modules/home-manager/vimiv-qt/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [vimiv-qt];
+  home.packages = with pkgs; [ vimiv-qt ];
 
   xdg.configFile."vimiv-qt/styles/base16-grayscale-dark".source = ./base16-grayscale-dark;
   xdg.configFile."vimiv-qt/vimiv.conf".source = ./vimiv.conf;
