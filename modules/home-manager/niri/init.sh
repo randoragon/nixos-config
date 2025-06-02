6e305ccc90e7ac50cdac42942d63de63c1b1f0a1
randoragon <randoragongamedev@gmail.com>
2025-06-02 10:51:44 +0200

    fix(swww): Initialize daemon correctly
    Strangely, `swww init` seems to have been removed.

--- modules/home-manager/niri/init.sh
+++ modules/home-manager/niri/init.sh
@@ -15,7 +15,7 @@ wlsunset &
 mkdir -p "${XDG_PICTURES_DIR}/Screenshots"
 
 # Start wallpaper manager
-swww init --no-cache
+swww-daemon --no-cache &
 swww img -t none ~/.config/wallpaper
 
 # Fix screen capture
