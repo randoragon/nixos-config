a06232f067ac7fd7645133233a7e425075e6d39b
randoragon <randoragongamedev@gmail.com>
2025-06-03 16:23:27 +0200

    fix: Attempt to disable command-not-found integration
    This fix does not work and the problem lies elsewhere. Committing for
    the sake of sharing.

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -15,6 +15,9 @@
   # https://github.com/nix-community/nix-ld
   programs.nix-ld.enable = true;
 
+  # Make sure CNF integration with nix-index is disabled
+  programs.command-not-found.enable = false;
+
   programs.gnupg.agent = {
     enable = true;
     pinentryPackage = pkgs.pinentry-gtk2;
