7efd010fd84f4ae67c8947f0eccfea07b9e5fca8
randoragon <randoragongamedev@gmail.com>
2025-06-29 13:06:23 +0200

    fix(lmms): Fix lmms binary path inside wrapper script

--- modules/home-manager/lmms/default.nix
+++ modules/home-manager/lmms/default.nix
@@ -1,8 +1,15 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   home.packages = with pkgs; [ lmms ];
 
   home.file.".local/bin/lmms" = {
-    source = ./lmms;
+    text = ''
+      #!/bin/sh -e
+
+      confhome="${config.xdg.configHome}/lmms"
+
+      [ ! -d "$confhome" ] && mkdir -p "$confhome"
+      ${pkgs.lmms}/bin/lmms -c "$confhome/lmmsrc.xml"
+    '';
     executable = true;
   };
 }
