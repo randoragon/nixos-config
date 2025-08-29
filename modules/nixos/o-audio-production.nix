03d1d6034c27fe73c337a432c7667490434f5d24
randoragon <randoragongamedev@gmail.com>
2025-08-29 12:23:07 +0200

    fix(o-audio-production): Increase Pipewire quantum to 256
    32 buffers size was glitchy like crazy, it's way too low.

--- modules/nixos/o-audio-production.nix
+++ modules/nixos/o-audio-production.nix
@@ -10,9 +10,9 @@
   services.pipewire.extraConfig.pipewire."92-low-latency" = {
     "context.properties" = {
       "default.clock.rate" = 48000;
-      "default.clock.quantum" = 32;
+      "default.clock.quantum" = 256;
       "default.clock.min-quantum" = 32;
-      "default.clock.max-quantum" = 32;
+      "default.clock.max-quantum" = 1024;
     };
   };
 }
