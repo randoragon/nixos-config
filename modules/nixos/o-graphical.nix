df272981d569c57de4057b39fee5d11e06f4eebd
randoragon <randoragongamedev@gmail.com>
2025-09-01 20:30:29 +0200

    fix: Remove river
    I've long moved to niri and I don't plan on going back (I am curious
    what the river developers are cooking with the upcoming new release,
    though).

--- modules/nixos/o-graphical.nix
+++ modules/nixos/o-graphical.nix
@@ -1,20 +1,13 @@
 # Optional graphical session support
-{
+{ pkgs, ... }: {
   services.libinput.enable = true;
-
-  # River should ideally be installed at the user-level.
-  # However, I encountered NVIDIA-related crashes when removing this
-  # system-wide enable. I assume this does something Home Manager's enabling
-  # option doesn't, which is why I'm keeping it.
-  programs.river = {
-    enable = true;
-    extraPackages = [];  # Don't use this; decide at the user-level
-  };
 
   # XDG Desktop Portal for PipeWire screensharing etc.
   # More precise configuration should be written at the user-level.
-  # This is more of a cautionary setting, in case the system-wide option does something Home Manager can't.
-  xdg.portal.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-wlr xdg-desktop-portal-gtk ];
+  };
 
   # Allow swaylock to authenticate (https://github.com/swaywm/sway/issues/2773#issuecomment-427570877)
   security.pam.services.swaylock = {
