627940be9c139da5da10f2e9ee52424e0b2d035c
randoragon <randoragongamedev@gmail.com>
2025-09-21 22:02:25 +0200

    chore(carla): Bring back unstable carla
    It's been a while and it's not broken anymore.

--- modules/home-manager/carla/default.nix
+++ modules/home-manager/carla/default.nix
@@ -1,5 +1,5 @@
-{ config, lib, spkgs, ... }: {
-  home.packages = with spkgs; [ carla ];  # TODO: switch back to unstable pkgs
+{ config, lib, pkgs, ... }: {
+  home.packages = with pkgs; [ carla ];  # TODO: switch back to unstable pkgs
 
   # Disable carla quit confirmation dialog and configure paths.
   # Use activation, because we want the config file to be writeable.
