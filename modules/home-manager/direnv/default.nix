c07a31a0688286ed1cee6e29f068a52115e3d3bf
randoragon <randoragongamedev@gmail.com>
2025-09-21 18:41:53 +0200

    fix(direnv): Disable "is taking a while to execute" warnings

--- modules/home-manager/direnv/default.nix
+++ modules/home-manager/direnv/default.nix
@@ -3,5 +3,10 @@
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
+
+    config = {
+      # Disable "... is taking a while to execute" messages
+      warn_timeout = 0;
+    };
   };
 }
