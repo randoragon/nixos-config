46c376a555517db9f4712f5874249e8ff2e0589d
randoragon <randoragongamedev@gmail.com>
2025-11-08 09:43:37 +0100

    refactor(o-graphical): Use new XDG Desktop Portal scheme
    In response to:
    
        evaluation warning: xdg-desktop-portal 1.17 reworked how portal implementations are loaded, you
                            should either set `xdg.portal.config` or `xdg.portal.configPackages`
                            to specify which portal backend to use for the requested interface.
    
                            https://github.com/flatpak/xdg-desktop-portal/blob/1.18.1/doc/portals.conf.rst.in
    
                            If you simply want to keep the behaviour in < 1.17, which uses the first
                            portal implementation found in lexicographical order, use the following:
    
                            xdg.portal.config.common.default = "*";
    
    I still don't understand why my screensharing works with just gtk and
    wlr, since Niri docs state that you need gnome for that
    (https://github.com/YaLTeR/niri/wiki/Important-Software), but whatever.

--- modules/nixos/o-graphical.nix
+++ modules/nixos/o-graphical.nix
@@ -7,6 +7,10 @@
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-wlr xdg-desktop-portal-gtk ];
+    config.common.default = [
+      "gtk"
+      "wlr"
+    ];
   };
 
   # Required to make some gtk features work
