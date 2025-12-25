3027b6dfdfa2fbe2851a546007a5c10f304d99cc
randoragon <randoragongamedev@gmail.com>
2025-12-25 13:32:58 +0100

    fix(nvidia): Pin driver to 580.119.02 for nixbox
    This is because my current GPU (GTX 1060) is too old for the latest
    drivers, it's recently been dropped to legacy support. I will need to
    buy a new GPU soonish...
    
    The two middle hashes don't seem to do anything, as in their values
    don't matter. But removing them introduces an error:
    
        Failed assertions:
        - This version of NVIDIA driver does not provide a GSP firmware.
    
    So whatever, I'll just leave them in.

--- hosts/nixbox/nvidia.nix
+++ hosts/nixbox/nvidia.nix
@@ -8,6 +8,14 @@
     powerManagement.finegrained = false;
     open = false;  # GTX 1060 is not supported
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    # https://nixos.wiki/wiki/Nvidia#Running_Specific_NVIDIA_Driver_Versions
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.119.02";
+      sha256_64bit = "sha256-gCD139PuiK7no4mQ0MPSr+VHUemhcLqerdfqZwE47Nc=";
+      sha256_aarch64 = "sha256-gCD139PuiK7no4mQ0MPSr+VHUemhcLqerdfqZwE47Nc=";
+      openSha256 = "sha256-sI/ly6gNaUw0QZFWWkMbrkSstzf0hvcdSaogTUoTecI=";
+      settingsSha256 = "sha256-sI/ly6gNaUw0QZFWWkMbrkSstzf0hvcdSaogTUoTecI=";
+      persistencedSha256 = lib.fakeSha256;
+    };
   };
 }
