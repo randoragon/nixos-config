9a4d1e612965504ed52aec9468e8f3fd4d4d215b
randoragon <randoragongamedev@gmail.com>
2026-01-31 19:07:07 +0100

    feat(o-virtualization): Add swtpm for TPM emulation

--- modules/nixos/o-virtualization.nix
+++ modules/nixos/o-virtualization.nix
@@ -2,7 +2,10 @@
 # https://nixos.wiki/wiki/Virt-manager
 { pkgs, ... }: {
   # Enable QEMU and Virt Manager
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.swtpm.enable = true;  # Windows 11 requires TPM 2.0
+  };
   programs.virt-manager.enable = true;
   environment.systemPackages = with pkgs; [
     qemu qemu_kvm
