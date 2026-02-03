2697032b76737018d0d22582d682507a7fe84eee
randoragon <randoragongamedev@gmail.com>
2026-02-03 12:04:05 +0100

    fix(nvim): Update treesitter configuration

--- modules/home-manager/nvim/nvim/lua/plugins/treesitter.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/treesitter.lua
@@ -1,58 +1,44 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
-treesitter = require("nvim-treesitter.configs")
+treesitter = require("nvim-treesitter")
 
-treesitter.setup {
-	ensure_installed = {
-		"awk",
-		"bash",
-		"c", "cpp", "c_sharp",
-		"corn",
-		"make", "cmake",
-		"diff",
-		"dockerfile",
-		"dot",
-		"gitcommit", "git_config", "gitignore", "git_rebase", "gitattributes",
-		"gnuplot",
-		"go", "gosum", "gowork",
-		"html", "css", "javascript", "typescript", "jsdoc",
-		"json", "yaml",
-		"latex", "bibtex",
-		"ledger",
-		"lua", "luadoc", "luap",
-		"markdown", "markdown_inline", "rst",
-		"nix",
-		"passwd",
-		"python",
-		"regex",
-		"ruby",
-		"rust",
-		"sql",
-		"toml",
-		"typst",
-		"vim", "vimdoc",
-		"zig",
-	},
+treesitter.setup()
+treesitter.install({
+	"awk",
+	"bash",
+	"c", "cpp", "c_sharp",
+	"corn",
+	"make", "cmake",
+	"diff",
+	"dockerfile",
+	"dot",
+	"gitcommit", "git_config", "gitignore", "git_rebase", "gitattributes",
+	"gnuplot",
+	"go", "gosum", "gowork",
+	"html", "css", "javascript", "typescript", "jsdoc",
+	"json", "yaml",
+	"latex", "bibtex",
+	"ledger",
+	"lua", "luadoc", "luap",
+	"markdown", "markdown_inline", "rst",
+	"nix",
+	"passwd",
+	"python",
+	"regex",
+	"ruby",
+	"rust",
+	"sql",
+	"toml",
+	"typst",
+	"vim", "vimdoc",
+	"zig",
+})
 
-	sync_install = false,
-	auto_install = false,
-	ignore_install = {},
-
-	highlight = {
-		enable = true,
-
-		-- Disable highlighting for files over 100KiB
-		disable = function(lang, buf)
-			local max_filesize = 100 * 1024
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-				return true
-			end
-		end,
-
-		additional_vim_regex_highlighting = false,
-	},
-
-	indent = {
-		enable = true,
-	},
-}
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "<filetype>" },
+	callback = function()
+		vim.treesitter.start()
+		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.wo[0][0].foldmethod = "expr"
+		vim.bo.indentexpr = "v:lua.vim.treesitter.indentexpr()"
+	end,
+})
