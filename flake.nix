0acda4c021dbfe9f9dd5aaf7f35c44d7e9015b70
randoragon <randoragongamedev@gmail.com>
2026-02-20 14:28:05 +0100

    build: Use `inputs.nixpkgs.follows = "nixpkgs"` everywhere
    I was not aware of it, but this is possible for every input and it
    solves the problem of updating input flakes all the time. Phew.

--- flake.nix
+++ flake.nix
@@ -4,20 +4,41 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-    rsid3.url = "github:randoragon/rsid3";
-    secrets.url = "github:randoragon/secrets";
-    lxmake.url = "github:randoragon/lxmake";
-    note.url = "github:randoragon/note";
-    music-tools.url = "github:randoragon/music-tools";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # https://github.com/nix-community/nix-index-database
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    rsid3 = {
+      url = "github:randoragon/rsid3";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    secrets = {
+      url = "github:randoragon/secrets";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lxmake = {
+      url = "github:randoragon/lxmake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    note = {
+      url = "github:randoragon/note";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    music-tools = {
+      url = "github:randoragon/music-tools";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, ... }@inputs: let
