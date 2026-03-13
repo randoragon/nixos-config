84e17e74e1e7839edf48ab647c9bd7f1ce10d5b5
randoragon <randoragongamedev@gmail.com>
2026-03-13 11:32:05 +0100

    fix(nvim): Configure treesitter properly
    My previous attempt at the migration to the new API was a failure and
    didn't work. Unfortunately you can't avoid more explicitness in the new
    API, but maybe it's a good thing.

--- modules/home-manager/nvim/nvim/lua/plugins/treesitter.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/treesitter.lua
@@ -1,45 +1,78 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 treesitter = require("nvim-treesitter")
 
-treesitter.setup()
-treesitter.install({
+local grammars_and_ftypes = {
 	"awk",
 	"bash",
-	"c", "cpp", "c_sharp",
+	"c", "cpp",
 	"corn",
 	"make", "cmake",
 	"diff",
 	"dockerfile",
 	"dot",
-	"gitcommit", "git_config", "gitignore", "git_rebase", "gitattributes",
+	"gitcommit", "gitignore", "gitattributes",
 	"gnuplot",
-	"go", "gosum", "gowork",
-	"html", "css", "javascript", "typescript", "jsdoc",
+	"go",
+	"html", "css", "javascript", "typescript",
 	"json", "yaml",
 	"kdl",
 	"latex", "bibtex",
 	"ledger",
-	"lua", "luadoc", "luap",
-	"markdown", "markdown_inline", "rst",
+	"lua",
+	"markdown", "rst",
 	"nix",
 	"passwd",
 	"python",
-	"regex",
 	"ruby",
 	"rust",
 	"sql",
 	"toml",
 	"typst",
-	"vim", "vimdoc",
+	"vim",
 	"zig",
-})
+}
+
+local additional_grammars = {
+	"c_sharp",
+	"git_config", "git_rebase",
+	"gosum", "gowork",
+	"jsdoc",
+	"luadoc", "luap",
+	"markdown_inline",
+	"regex",
+	"vimdoc",
+}
+
+local additional_ftypes = {
+	"sh", "zsh",
+	"cs",
+	"gitconfig", "gitrebase",
+	"viminfo",
+	"help",
+}
+
+-- Merge config tables
+local grammars = {}
+local ftypes = {}
+for _, v in ipairs(grammars_and_ftypes) do
+	grammars[#grammars + 1] = v
+	ftypes[#ftypes + 1] = v
+end
+for _, v in ipairs(additional_grammars) do
+	grammars[#grammars + 1] = v
+end
+for _, v in ipairs(additional_ftypes) do
+	ftypes[#ftypes + 1] = v
+end
+
+treesitter.install(grammars)
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "<filetype>" },
+	pattern = ftypes,
 	callback = function()
 		vim.treesitter.start()
 		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 		vim.wo[0][0].foldmethod = "expr"
-		vim.bo.indentexpr = "v:lua.vim.treesitter.indentexpr()"
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 	end,
 })
