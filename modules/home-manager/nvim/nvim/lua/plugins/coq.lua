5c0a143f886912615d748d58e8cb80421335b3ba
randoragon <randoragongamedev@gmail.com>
2026-06-20 17:59:22 +0200

    fix(nvim): Update COQ settings to v2

--- modules/home-manager/nvim/nvim/lua/plugins/coq.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/coq.lua
@@ -1,13 +1,11 @@
 -- https://github.com/ms-jpq/coq_nvim/blob/coq/docs/CONF.md
 vim.g.coq_settings = {
-	auto_start = true,
-	xdg = true,
 	["display.statusline.helo"] = false,
+	["display.icons.mode"] = "none",
 
 	clients = {
 		snippets = {
 			enabled = false,
-			warn = {},
 			user_path = os.getenv("XDG_CONFIG_HOME") .. "/nvim/lua/plugins/coq-user-snippets/",
 		},
 	},
