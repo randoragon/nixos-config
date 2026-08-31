2bd2c9ff07e48508e7399183f2d56778f8cbc209
randoragon <randoragongamedev@gmail.com>
2026-08-31 16:16:51 +0200

    feat: Add Cachix configuration

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,12 +1,30 @@
-{
-  # Enable flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+{ pkgs, ... }: {
+
+  nix.settings = {
+    # Enable flakes
+    experimental-features = [ "nix-command" "flakes" ];
+
+    # Optimize the store automatically
+    auto-optimise-store = true;
+
+    # Authorize these users to add stuff to substituters (this is equivalent to
+    # having root access due to the ability to swap binary caches).
+    trusted-users = [ "pcache" ];
+
+    # Set up binary caches
+    substituters = [
+      "https://cache.nixos.org"
+      "https://pcache.cachix.org"
+    ];
+    trusted-public-keys = [
+      # The public key of cache.nixos.org is built-in
+      "pcache.cachix.org-1:bvgSb3beNZ7Q0DTg8VqI47Dbi6Ok4VJWxmypa4KLmO4="
+    ];
+  };
 
   # Allow instaling non-free packages
   nixpkgs.config.allowUnfree = true;
 
-  # Optimize the store automatically
-  nix.settings.auto-optimise-store = true;
 
   # Enable nh and auto-cleaning
   programs.nh = {
@@ -25,4 +43,7 @@
     enableZshIntegration = false;
     enableFishIntegration = false;
   };
+
+  # Install cachix CLI tool
+  environment.systemPackages = with pkgs; [ cachix ];
 }
